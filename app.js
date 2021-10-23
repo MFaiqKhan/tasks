345afe99105add3135638d76093061c17df6216f
MFaiqKhan <faiqkhanthinkcentre@yahoo.com>
2021-10-23 17:37:07 +0500

    updated

--- app.js
+++ app.js
@@ -11,16 +11,29 @@ document.write("</br>")
 let crntHrs = today.getHours()
 let crntMints = today.getMinutes()
 let crntSecs = today.getSeconds()
+let ampm = (crntHrs >= 12)? "PM" : "AM"
+
+if(crntHrs >= 12) {
+  crntHrs = crntHrs - 12
+} else {
+  crntHrs = crntHrs
+}
+
+if(crntMints <= 9){
+  crntMints = `0${crntMints}`
+} else {
+  crntMints = crntMints
+}
 
 //let AM_PM = (crntHrs >= 12)? "AM": "PM"
-function AM_PM(_date){
+/* function AM_PM(_date){
 var ampm = hours >= 12 ? 'pm' : 'am';
   hours = hours % 12;
   hours = hours ? hours : 12; // the hour '0' should be '12'
   minutes = minutes < 10 ? '0'+ minutes : minutes;
   return ampm
-}
+} */
 
 document.write("</br>")
-document.write(`Current Time is : ${crntHrs} ${AM_PM(new Date())} : ${crntMints} : ${crntSecs}`);
+document.write(`Current Time is : ${crntHrs} ${ampm} : ${crntMints} : ${crntSecs}`);
 
